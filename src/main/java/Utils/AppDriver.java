5096e272abc2198e674098b55dfc10ac3c8a53ae
Khalid Mohamed <73295536+khalid-moh193@users.noreply.github.com>
2022-03-10 13:28:49 +0200

    Update AppDriver

--- src/main/java/Utils/AppDriver.java
+++ src/main/java/Utils/AppDriver.java
@@ -1,13 +1,11 @@
 package Utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,7 +14,7 @@ public class AppDriver {
     WebDriver driver;
 
     public WebDriver SeleniumDriverSetup(String browser) throws Exception {
-//         Check if parameter passed from web.xml is 'firefox'
+//         Check if parameter passed from testng.xml is 'firefox'
         if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             FirefoxOptions options = new FirefoxOptions();
@@ -28,7 +26,7 @@ public class AppDriver {
             return driver;
         }
 
-//        Check if parameter passed from web.xml is 'chrome'
+//        Check if parameter passed from testng.xml is 'chrome'
         else if (browser.equalsIgnoreCase("chrome")) {
 
             WebDriverManager.chromedriver().setup();
@@ -40,25 +38,10 @@ public class AppDriver {
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
             driver.manage().window().maximize();
             return driver;
-        }
-//        Check if parameter passed from web.xml is 'safari'
-        else if (browser.equalsIgnoreCase("safari")) {
-
-//You must enable 'Allow Remote Automation' option in Safari's Develop menu to control Safari via WebDriver
-            DriverManagerType safari = DriverManagerType.SAFARI;
-            WebDriverManager.getInstance(safari).setup();
-            driver = new SafariDriver();
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            driver.manage().window().maximize();
-
-            return driver;
         } else {
             // If no browser passed throw exception
             throw new Exception("Browser is not correct");
         }
     }
 
-    public void driverQuit() {
-        driver.quit();
-    }
 }
